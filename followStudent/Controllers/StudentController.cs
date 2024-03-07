fea0b6fa0df59dc5ff84bd8f3e7295ab5ba891e7
Mehmet4602can <mehmetcelik4602@gmail.com>
2024-03-07 11:18:01 +0300

    Proje dosyası ekle.

---
+++ followStudent/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using followStudent.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace followStudent.Controllers
+{
+    public class StudentController : Controller
+    { 
+        static List<StudentModel> students = new List<StudentModel>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddStudent(StudentModel student)
+        {
+            students.Add(student);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Addliste(StudentModel student) 
+        {
+            return View(students);
+        }
+    }
+}
---
+++ followStudent/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace followStudent.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }   
+        public string? Surname { get; set; }
+        public string? classes { get; set; }
+    }
+}
